cb8824382cd852e5cd1de248894ce2566c4f3673
Julian Wayte <julianwayte@gmail.com>
2024-01-14 15:49:13 -0700

    Update show_platform.py

--- show_platform.py
+++ show_platform.py
@@ -1,1 +1,13 @@
+import platform
+
+# determine platform (Linux/Windows)
+platform = str(platform.platform())
+print('Platform: ', platform)
+
+# run uname -a to get more details of the OS
+process = subprocess.Popen("uname -a", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+unameOutput = str(process.communicate()[0])
+process.wait()
+exitCode = process.returncode
+print(unameOutput)
 
