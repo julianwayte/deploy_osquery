3d452ea3ea5b7934280d8d33f76064a7a08be452
Julian Wayte <julianwayte@gmail.com>
2024-01-15 19:23:16 -0700

    Update show_platform.py

--- show_platform.py
+++ show_platform.py
@@ -5,7 +5,7 @@ import subprocess
 platform = str(platform.platform())
 print('Platform: ', platform)
 
-# If Linux then run 'uname -a' to get more details of the OS
+# On Linux run 'uname -a' to get more details of the OS
 process = subprocess.Popen("uname -a", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 unameOutput = str(process.communicate()[0])
 process.wait()
